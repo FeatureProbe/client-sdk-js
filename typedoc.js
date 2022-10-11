b53605d9b8e6ffb0fc310a99c9cbc1a46756abab
panchongyang <yourhand1025@163.com>
2022-10-11 14:11:02 +0800

    fix(docs): fix docs
    Changed document output path

--- typedoc.js
+++ typedoc.js
@@ -3,7 +3,7 @@ const pkg = require('./package.json');
 const PKG_VERSION = pkg.version;
 
 module.exports = {
-  out: './doc',
+  out: './docs',
   name: 'FeatureProbe Client Side SDK for JavaScript  (' + PKG_VERSION + ')',
   readme: 'none',
   entryPoints: ["./src/index.ts"]
