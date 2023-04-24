1af41c3b263d0c8d5d98a002b2ab238b01d3755e
frankzhaopeng <poryzhao@featureprobe.com>
2023-04-24 17:03:17 +0800

    :hammer:fix: issue

--- test/flushEvent.test.ts
+++ test/flushEvent.test.ts
@@ -1,19 +1,23 @@
 import flushEventBeforPageUnload from "../src/flushEvents";
 import { EventRecorder } from "../src/EventRecorder";
 
-test("flushEventBeforPageUnload should add event listeners and flush events on page unload", () => {
-    // Make a eventRecorder instance
-    const eventRecorder = new EventRecorder("sdkKey", "https://www.com", 1000);
 
-    // Spy on the flush method of the mock EventRecorder object
-    const flushSpy = jest.spyOn(eventRecorder, "flush");
-  
-    // Call the flushEventBeforPageUnload function with the mock EventRecorder object
-    flushEventBeforPageUnload(eventRecorder);
-  
-    // Simulate a page unload event
-    window.dispatchEvent(new Event("unload"));
-  
-    // Expect the EventRecorder's flush method to have been called
-    expect(flushSpy).toHaveBeenCalled();
-  });
+
+test("flushEventBeforPageUnload should add event listeners and flush events on page unload", () => {
+  const INTERVAL = 1000;
+
+  // Make a eventRecorder instance
+  const eventRecorder = new EventRecorder("sdkKey", "https://www.com", INTERVAL);
+
+  // Spy on the flush method of the mock EventRecorder object
+  const flushSpy = jest.spyOn(eventRecorder, "flush");
+
+  // Call the flushEventBeforPageUnload function with the mock EventRecorder object
+  flushEventBeforPageUnload(eventRecorder);
+
+  // Simulate a page unload event
+  window.dispatchEvent(new Event("unload"));
+
+  // Expect the EventRecorder's flush method to have been called
+  expect(flushSpy).toHaveBeenCalled();
+});
